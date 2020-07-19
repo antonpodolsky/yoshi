136f1e7caeb5efae16e5df3086c27a624eaf39f7
Amit Dahan <amitda@wix.com>
2020-07-19 17:20:21 +0300

    Enable fedops integration by checking fedops.json existence

--- packages/create-yoshi-app/templates/flow-bm/typescript/src/pages/index.tsx
+++ packages/create-yoshi-app/templates/flow-bm/typescript/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect, Suspense } from 'react';
 import { notifyViewFinishedLoading } from '@wix/business-manager-api';
-import { useExperiments } from 'yoshi-flow-bm-runtime';
+import { useExperiments, useFedops } from 'yoshi-flow-bm-runtime';
 import t from '../../translations/en.json';
 
 const Experiment: FC = () => {
@@ -14,8 +14,11 @@ const Experiment: FC = () => {
 };
 
 const Index: FC = () => {
+  const fedops = useFedops();
+
   useEffect(() => {
     notifyViewFinishedLoading('{%projectName%}.pages.index');
+    fedops.appLoaded();
   }, []);
 
   return (
